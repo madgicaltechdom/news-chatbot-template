5163fd1cba9ea642326b7de5a925d4e99eb4a90b
Aarti Thakur <aarti.thakur@madgicaltechdom.com>
2024-04-10 16:25:26 +0530

    test cases

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -12,6 +12,7 @@ import { InjectRepository, getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../src/model/user.entity';
 import { LocalizationService } from '../src/localization/localization.service';
+import { Body } from '@nestjs/common';
 
 dotenv.config();
 
@@ -104,7 +105,6 @@ describe('AppController', () => {
       language: 'English',
     });
 
-
     jest.spyOn(userService, 'findUserByMobileNumber').mockResolvedValueOnce({
       id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
       mobileNumber: '1234567890',
@@ -114,16 +114,14 @@ describe('AppController', () => {
       language: 'English',
     });
 
-    jest
-      .spyOn(userService, 'saveButtonResponse')
-      .mockResolvedValueOnce({
-        id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
-        mobileNumber: '1234567890',
-        botID: '0261394256641954',
-        userContext: '',
-        buttonResponse: 'Sports',
-        language: 'English',
-      });
+    jest.spyOn(userService, 'saveButtonResponse').mockResolvedValueOnce({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
 
     jest
       .spyOn(LocalizationService, 'getLocalisedString')
@@ -172,122 +170,246 @@ describe('AppController', () => {
     );
   });
 
+  // When provided with a valid 'button_response' containing a category or subcategory, sends news as an article carousel and goes back to main menu if subcategory is selected
+  it('should send news as an article carousel and go back to main menu if subcategory is selected', async () => {
+    // Arrange
+    const body = {
+      from: '1234567890',
+      text: null,
+      button_response: { body: 'Sports1' },
+      persistent_menu_response: null,
+    };
 
-      // When provided with a valid 'button_response' containing a category or subcategory, sends news as an article carousel and goes back to main menu if subcategory is selected
-      it('should send news as an article carousel and go back to main menu if subcategory is selected', async () => {
-        // Arrange
-        const body = {
-          from: '1234567890',
-          text: null,
-          button_response: { body: 'Sports1' },
-          persistent_menu_response: null,
-        };
-
-        jest.spyOn(userService.userRepository, 'findOne').mockResolvedValue({
-          id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
-          mobileNumber: '1234567890',
-          botID: '0261394256641954',
-          userContext: '',
-          buttonResponse: 'शिक्षा',
-          language: 'English',
-        });
-    
-        jest.spyOn(userService, 'findUserByMobileNumber').mockResolvedValueOnce({
-          id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
-          mobileNumber: '1234567890',
-          botID: '0261394256641954',
-          userContext: '',
-          buttonResponse: 'Sports',
-          language: 'English',
-        });
-  
-        // Mock the necessary methods
-        jest.spyOn(LocalizationService, 'getLocalisedString').mockResolvedValueOnce(english);
-        jest.spyOn(messageService, 'getCategoryID').mockResolvedValueOnce('49');
-        jest.spyOn(khabriMediaService, 'sendNewsAsArticleCarousel').mockResolvedValueOnce(2);
-        jest.spyOn(messageService, 'goBackToMainMenu').mockResolvedValueOnce({ id: '83d9db82-b3b0-4802-b2d8-a92f481435c6' });
-  
-        // Act
-        const result = await chatbotService.processMessage(body);
-  
-        // Assert
-        expect(result).toBe('ok');
-        expect(userService.findUserByMobileNumber).toHaveBeenCalledWith('1234567890');
-        expect(LocalizationService.getLocalisedString).toHaveBeenCalledWith('English');
-        expect(messageService.getCategoryID).toHaveBeenCalledWith('Sports1', 'English');
-        expect(khabriMediaService.sendNewsAsArticleCarousel).toHaveBeenCalledWith('English', process.env.BOT_ID, '1234567890', '49', 'Sports1', 1, 2);
-        expect(messageService.goBackToMainMenu).toHaveBeenCalledWith('1234567890', 'English');
-      });
-
-          // When given a valid 'button_response' in the request body with the value of 'Go Back To Main Menu', it sends the category buttons to the user.
-    it('should send category buttons when given valid \'button_response\' with value \'Go Back To Main Menu\'', async () => {
-      const body = {
-        from: '1234567890',
-        text: null,
-        button_response: { body: 'Go Back To Main Menu' },
-        persistent_menu_response: null,
-      };
-
-      jest.spyOn(userService.userRepository, 'findOne').mockResolvedValue({
-        id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
-        mobileNumber: '1234567890',
-        botID: '0261394256641954',
-        userContext: '',
-        buttonResponse: 'शिक्षा',
-        language: 'English',
-      });
-  
-      jest.spyOn(userService, 'findUserByMobileNumber').mockResolvedValueOnce({
-        id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
-        mobileNumber: '1234567890',
-        botID: '0261394256641954',
-        userContext: '',
-        buttonResponse: 'Sports',
-        language: 'English',
-      });
-
-      // Act
-      const result = await chatbotService.processMessage(body);
-
-      // Assert
-      expect(result).toBe('ok');
-      expect(messageService.categoryButtons).toHaveBeenCalledWith(body.from, 'English');
+    jest.spyOn(userService.userRepository, 'findOne').mockResolvedValue({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'शिक्षा',
+      language: 'English',
     });
 
-        // When given a valid 'button_response' in the request body with the value of 'Browse Other Categories', it sends the sub-category buttons for the previously selected category to the user.
-        it('should send sub-category buttons when given valid \'button_response\' with value \'Browse Other Categories\'', async () => {
+    jest.spyOn(userService, 'findUserByMobileNumber').mockResolvedValueOnce({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
 
-          const body = {
-            from: '1234567890',
-            text: null,
-            button_response: { body: 'Browse Other Categories' },
-            persistent_menu_response: null,
-          };
-    
-          jest.spyOn(userService.userRepository, 'findOne').mockResolvedValue({
-            id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
-            mobileNumber: '1234567890',
-            botID: '0261394256641954',
-            userContext: '',
-            buttonResponse: 'Sports',
-            language: 'English',
-          });
-      
-          jest.spyOn(userService, 'findUserByMobileNumber').mockResolvedValueOnce({
-            id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
-            mobileNumber: '1234567890',
-            botID: '0261394256641954',
-            userContext: '',
-            buttonResponse: 'Sports',
-            language: 'English',
-          });
-    
-          jest.spyOn(messageService, 'sub_categoryButtons');        
-          // Act
-          await chatbotService.processMessage(body);
-    
-          // Assert
-          expect(messageService.sub_categoryButtons).toHaveBeenCalledWith('1234567890', 'English', 'Sports');
-          
-        });
+    // Mock the necessary methods
+    jest
+      .spyOn(LocalizationService, 'getLocalisedString')
+      .mockResolvedValueOnce(english);
+    jest.spyOn(messageService, 'getCategoryID').mockResolvedValueOnce('49');
+    jest
+      .spyOn(khabriMediaService, 'sendNewsAsArticleCarousel')
+      .mockResolvedValueOnce(2);
+    jest
+      .spyOn(messageService, 'goBackToMainMenu')
+      .mockResolvedValueOnce({ id: '83d9db82-b3b0-4802-b2d8-a92f481435c6' });
+
+    // Act
+    const result = await chatbotService.processMessage(body);
+
+    // Assert
+    expect(result).toBe('ok');
+    expect(userService.findUserByMobileNumber).toHaveBeenCalledWith(
+      '1234567890',
+    );
+    expect(LocalizationService.getLocalisedString).toHaveBeenCalledWith(
+      'English',
+    );
+    expect(messageService.getCategoryID).toHaveBeenCalledWith(
+      'Sports1',
+      'English',
+    );
+    expect(khabriMediaService.sendNewsAsArticleCarousel).toHaveBeenCalledWith(
+      'English',
+      process.env.BOT_ID,
+      '1234567890',
+      '49',
+      'Sports1',
+      1,
+      2,
+    );
+    expect(messageService.goBackToMainMenu).toHaveBeenCalledWith(
+      '1234567890',
+      'English',
+    );
+  });
+
+  // When given a valid 'button_response' in the request body with the value of 'Go Back To Main Menu', it sends the category buttons to the user.
+  it("should send category buttons when given valid 'button_response' with value 'Go Back To Main Menu'", async () => {
+    const body = {
+      from: '1234567890',
+      text: null,
+      button_response: { body: 'Go Back To Main Menu' },
+      persistent_menu_response: null,
+    };
+
+    jest.spyOn(userService.userRepository, 'findOne').mockResolvedValue({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'शिक्षा',
+      language: 'English',
+    });
+
+    jest.spyOn(userService, 'findUserByMobileNumber').mockResolvedValueOnce({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
+
+    // Act
+    const result = await chatbotService.processMessage(body);
+
+    // Assert
+    expect(result).toBe('ok');
+    expect(messageService.categoryButtons).toHaveBeenCalledWith(
+      body.from,
+      'English',
+    );
+  });
+
+  // When given a valid 'button_response' in the request body with the value of 'Browse Other Categories', it sends the sub-category buttons for the previously selected category to the user.
+  it("should send sub-category buttons when given valid 'button_response' with value 'Browse Other Categories'", async () => {
+    const body = {
+      from: '1234567890',
+      text: null,
+      button_response: { body: 'Browse Other Categories' },
+      persistent_menu_response: null,
+    };
+
+    jest.spyOn(userService.userRepository, 'findOne').mockResolvedValue({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
+
+    jest.spyOn(userService, 'findUserByMobileNumber').mockResolvedValueOnce({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
+
+    jest.spyOn(messageService, 'sub_categoryButtons');
+    // Act
+    await chatbotService.processMessage(body);
+
+    // Assert
+    expect(messageService.sub_categoryButtons).toHaveBeenCalledWith(
+      '1234567890',
+      'English',
+      'Sports',
+    );
+  });
+
+  // When the language button is clicked, save the language and show the main menu in the selected language.
+  it('should show the language options when persistent menu is clicked', async () => {
+    // Arrange
+    const body = {
+      from: '1234567890',
+      text: null,
+      button_response: null,
+      persistent_menu_response: { body: 'English' },
+    };
+
+    jest.spyOn(userService.userRepository, 'findOne').mockResolvedValue({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
+
+    jest.spyOn(userService, 'findUserByMobileNumber').mockResolvedValueOnce({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
+
+    jest
+      .spyOn(messageService, 'languageButtons')
+      .mockResolvedValueOnce({ id: '83d9db82-b3b0-4802-b2d8-a92f481435c6' });
+
+    // Act
+    const result = await chatbotService.processMessage(body);
+
+    // Assert
+    expect(result).toBe('ok');
+    expect(messageService.languageButtons).toHaveBeenCalledWith(
+      '1234567890',
+      'English',
+    );
+  });
+
+  // When the language button is clicked, save the language and show the main menu in the selected language.
+  it('should save the language and show the main menu in the selected language when the language button is clicked', async () => {
+    // Arrange
+    const body = {
+      from: '1234567890',
+      text: null,
+      button_response: { body: 'English' },
+      persistent_menu_response: null,
+    };
+
+    jest.spyOn(userService.userRepository, 'save').mockResolvedValue({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
+
+    jest.spyOn(userService.userRepository, 'findOne').mockResolvedValue({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
+
+    jest.spyOn(userService, 'findUserByMobileNumber').mockResolvedValueOnce({
+      id: 'a76b9089-9583-48f9-a29d-87b36aa6c82c',
+      mobileNumber: '1234567890',
+      botID: '0261394256641954',
+      userContext: '',
+      buttonResponse: 'Sports',
+      language: 'English',
+    });
+
+    // Act
+    const result = await chatbotService.processMessage(body);
+
+    // Assert
+    expect(result).toBe('ok');
+    // expect(userService.saveLanguage).toHaveBeenCalledWith(
+    //   '1234567890',
+    //   process.env.BOT_ID,
+    //   'English',
+    // );
+    expect(messageService.categoryButtons).toHaveBeenCalledWith(
+      '1234567890',
+      'English',
+    );
+  });
 });
